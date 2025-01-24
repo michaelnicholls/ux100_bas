ef70f266a7dff5f3d42311ee3b46f93cce6d5463
Michael W Nicholls <i016416@gmail.com>
2025-01-24 03:40:24 +0000

    first commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("mn.mnapp.controller.Main", {
+        onInit() {
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"mn/mnapp/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
